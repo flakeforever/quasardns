163f80267b553508a3f6e55e0e4605799d589757
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-06-09 18:49:50 +0800

    Update openssl build script

--- scripts/build-openssl.sh
+++ scripts/build-openssl.sh
@@ -2,20 +2,27 @@
 
 cd lib/openssl
 
+export CC=aarch64-linux-gnu-gcc
+export CXX=aarch64-linux-gnu-g++
+export AR=aarch64-linux-gnu-ar
+export RANLIB=aarch64-linux-gnu-ranlib
+
+export PATH=/usr/bin:/usr/aarch64-linux-gnu:$PATH
+
 if [ "$1" = "shared" ]; then
     if [ ! -f "libcrypto.so" ] || [ ! -f "libssl.so" ]; then
         # configure OpenSSL
-        ./config -fPIC shared --release
+        ./Configure linux-aarch64 -fPIC shared --release --cross-compile-prefix=aarch64-linux-gnu-
 
         # build OpenSSL
-        make -j4
+        make -j$(nproc)
     fi
 elif [ "$1" = "no-shared" ]; then
     if [ ! -f "libcrypto.a" ] || [ ! -f "libssl.a" ]; then
         # configure OpenSSL
-        ./config -fPIC no-shared --release
+        ./Configure linux-aarch64 -fPIC no-shared --release --cross-compile-prefix=aarch64-linux-gnu-   
 
         # build OpenSSL
-        make -j4
+        make -j$(nproc)
     fi
 fi
